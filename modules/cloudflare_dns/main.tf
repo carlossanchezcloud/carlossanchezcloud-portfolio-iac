39c8358b4df1d5edc59c6542fdfb0da22d7802b0
Carlos Sánchez <sanchezcarlos.ti@gmail.com>
2026-02-21 02:33:49 -0500

    allow_overwrite DNS y plan final validado 19 recursos

--- modules/cloudflare_dns/main.tf
+++ modules/cloudflare_dns/main.tf
@@ -1,4 +1,3 @@
-
 # modules/cloudflare_dns/main.tf
 terraform {
   required_providers {
@@ -9,21 +8,18 @@ terraform {
   }
 }
 
-# modules/cloudflare_dns/main.tf
-# Crea los registros DNS en Cloudflare
-# que apuntan el dominio a CloudFront
-
 # ─────────────────────────────────────────
 # 1. Registro raíz
 #    carlossanchezcloud.com → CloudFront
 # ─────────────────────────────────────────
 resource "cloudflare_record" "root" {
-  zone_id = var.cloudflare_zone_id
-  name    = "@"
-  content = var.cloudfront_domain_name
-  type    = "CNAME"
-  ttl     = 1
-  proxied = false
+  zone_id         = var.cloudflare_zone_id
+  name            = "@"
+  content         = var.cloudfront_domain_name
+  type            = "CNAME"
+  ttl             = 1
+  proxied         = false
+  allow_overwrite = true
 }
 
 # ─────────────────────────────────────────
@@ -31,10 +27,11 @@ resource "cloudflare_record" "root" {
 #    www.carlossanchezcloud.com → CloudFront
 # ─────────────────────────────────────────
 resource "cloudflare_record" "www" {
-  zone_id = var.cloudflare_zone_id
-  name    = "www"
-  content = var.cloudfront_domain_name
-  type    = "CNAME"
-  ttl     = 1
-  proxied = false
+  zone_id         = var.cloudflare_zone_id
+  name            = "www"
+  content         = var.cloudfront_domain_name
+  type            = "CNAME"
+  ttl             = 1
+  proxied         = false
+  allow_overwrite = true
 }
